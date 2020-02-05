386a7c744668987f7e1e738313734e53bb33ccb2
Jan Livny <janlabananeefc@gmail.com>
2020-02-05 13:05:15 +0400

    added signin app and set it up

--- sites_for_mc/sites_for_mc/urls.py
+++ sites_for_mc/sites_for_mc/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-
+from signin.views import home_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
